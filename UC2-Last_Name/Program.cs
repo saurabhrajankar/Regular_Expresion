424cff19619249e2683abef6f573a97d251170dc
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-21 11:23:48 +0530

    Enter calid last name

---
+++ UC2-Last_Name/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC2_Last_Name
+{
+    public class RegexV
+    {
+        public void LastNameValidation()
+        {
+            Console.WriteLine("Enter Last Name");
+            string Lastname = Console.ReadLine();
+            string lastname = "^[A-Z]{1}[a-z]{2,}$";//writing the expression as we are needed
+            Regex regex1 = new Regex(lastname);//creating a regex named obj with regular lastname
+            if (regex1.IsMatch(Lastname))//creating a regex named obj with regular firstname
+            {
+                Console.Write("Name is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter First Letter Capital \n");
+            }
+        }
+    }
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            RegexV obj = new RegexV();
+            obj.LastNameValidation();
+            Console.ReadLine();
+        }
+    }
+}
