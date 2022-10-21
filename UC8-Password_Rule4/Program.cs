be6b7d3882b85a3197899df70b9f42bf3b47fa6a
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-21 11:28:10 +0530

    Atleast one special ch.

---
+++ UC8-Password_Rule4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC8_Password_Rule4
+{
+    public class RegexV
+    {
+        public void EmailSample()
+        {
+            Console.WriteLine("Enter Email Id");
+            string email = Console.ReadLine();
+            string emailFormat = "[A-Za-z0-9](.[-+a-z0-9A-Z])+@[A-Za-z0-9]+.[a-z]{2,4}(.[a-z]{2,3})?$";//writing the expression as we are needed
+            Regex regex1 = new Regex(emailFormat);                        //creating a regex named obj with regular firstname   
+            if (regex1.IsMatch(email))                                   //if the word matches the regex          
+            {
+                Console.Write("Given Email ID is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter Email ID in Proper Format \n");
+            }
+        }
+    }
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            RegexV obj = new RegexV();
+            obj.EmailSample();
+            Console.ReadLine();
+        }
+    }
+}
