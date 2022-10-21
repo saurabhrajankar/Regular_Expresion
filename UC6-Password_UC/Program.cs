c8c05d0f8a54b6d5338f9c20c211b61d6657f0b9
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-21 11:26:26 +0530

    At least one upper case

---
+++ UC6-Password_UC/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC6_Password_UC
+{
+    public class RegexV
+    {
+        public void PasswordRule2()
+        {
+            Console.WriteLine("Enter Password :");
+            var Password = Console.ReadLine();
+            var PasswordFormat = "[A-Z]{1}[a-z]{8,}";    //writing the expression as we are needed
+            Regex regex1 = new Regex(PasswordFormat);   //creating a regex named obj with regular PasswordFormat
+            if (regex1.IsMatch(Password))               //if the word matches the regex
+            {
+                Console.Write("Given Password is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter Password in Proper Format \n");
+            }
+        }
+    }
+   
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegexV obj = new RegexV();
+            obj.PasswordRule2();
+            Console.ReadLine();
+        }
+    }
+}
