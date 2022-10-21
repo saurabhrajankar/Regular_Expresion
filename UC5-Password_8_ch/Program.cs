b2ee2a8b7423c834ab61fdd7b1412e72d80283f7
saurabhrajankar <112363658+saurabhrajankar@users.noreply.github.com>
2022-10-21 11:25:48 +0530

    Enter the minimum 8ch password

---
+++ UC5-Password_8_ch/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UC5_Password_8_ch
+{
+    public class RegexV
+    {
+        public void Password()
+        {
+            Console.WriteLine("Enter Password :");
+            var Password = Console.ReadLine();
+            var PasswordFormat = "[a-z]{8,}";          //writing the expression as we are needed
+            Regex regex1 = new Regex(PasswordFormat);  //creating a regex named obj with regular PasswordFormat
+            if (regex1.IsMatch(Password))             //if the word matches the regex
+            {
+                Console.Write("Given Password is Valid \n");
+            }
+            else
+            {
+                Console.Write("Please Enter Password in Proper Format \n");
+            }
+        }
+    }
+    internal class main
+    {
+        static void Main(string[] args)
+        {
+            RegexV obj = new RegexV();
+            obj.Password();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ UC5-Password_8_ch/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UC5-Password_8_ch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("UC5-Password_8_ch")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("71fe6d89-b2fc-41a7-a13a-04f80a51a1e9")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
